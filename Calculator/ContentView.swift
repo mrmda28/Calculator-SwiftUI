355dbffdee2edcd0db85510c3e010fd277e345f5
mrmda28 <mrmda28@gmail.com>
2021-07-31 23:44:47 +0300

    Initial commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Dmitriy Maslennikov on 31/07/2021.
+//  Copyright © 2021 mrmda28. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
